889b796a1e5180996ed2503d3d67f9c4cf312125
ryo-1026 <ryousuke19881026@gmail.com>
2025-01-05 23:22:47 +0900

    商品関連

--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue'
+
+
 
 defineProps({
     errors: Object
@@ -18,6 +21,7 @@ const submitFunction = () => {
 
 
 <template>
+      <BreezeValidationErrors :errors="errors"/>
     <form @submit.prevent="submitFunction">
         <input type="text" name="title" v-model="form.title"><br>
         <div v-if="errors.title">{{ errors.title }}</div>
