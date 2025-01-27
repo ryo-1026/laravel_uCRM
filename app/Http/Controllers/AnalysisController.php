c2e4de5448e5b7f1cd02a8c4689cde7d53cfd930
ryo-1026 <ryousuke19881026@gmail.com>
2025-01-27 18:27:03 +0900

    decile

--- app/Http/Controllers/AnalysisController.php
+++ app/Http/Controllers/AnalysisController.php
@@ -11,26 +11,6 @@ class AnalysisController extends Controller
 {
     public function index()
     {
-        // $startDate = '2024-08-01';
-        // $endDate = '2024-08-31';
-
-        // $period = Order::betweenDate($startDate, $endDate)
-        //     ->groupBy('id')
-        //     ->selectRaw('id, sum(subtotal) as total, customer_name, status, created_at')
-        //     ->orderBy('created_at')
-        //     ->paginate(50);
-
-
-        // $subQuery = Order::betweenDate($startDate, $endDate)
-        //     ->where('status', true)
-        //     ->groupBy('id')
-        //     ->selectRaw('id, sum(subtotal) as totalPerPurchase, DATE_FORMAT(created_at,"%Y%m%d") as date');
-
-        // $data = DB::table($subQuery)
-        //     ->groupBy('date')
-        //     ->selectRaw('date, sum(totalPerPurchase) as total')
-        //     ->get();
-
         return Inertia::render('Analysis');
     }
 }
